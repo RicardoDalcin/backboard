e37cd2eb32927890725d514b6cce10b40f8d0f35
Ricardo Dalcin <ricardo.dalcinh@gmail.com>
2025-04-15 19:16:34 -0300

    🚧 Start court draw

--- src/engine/Visualization.ts
+++ src/engine/Visualization.ts
@@ -1,3 +1,22 @@
+// All units are in feet
+
+// 50mm (0.16 ft)
+const LINE_WIDTH = 0.16;
+
+// Distance between sideline and three-point line
+const THREE_POINT_LINE_DISTANCE = 3;
+
+// Radius of the three-point line from the basket
+const THREE_POINT_LINE_RADIUS = 23.75;
+
+const BACKBOARD_DISTANCE_TO_BACKLINE = 4;
+const BASKET_DISTANCE_TO_BACKLINE = 5.25;
+
+const COURT_LENGTH_FT = 94 / 2;
+const COURT_WIDTH_FT = 50;
+
+const COURT_ASPECT_RATIO = COURT_LENGTH_FT / COURT_WIDTH_FT;
+
 export class VisualizationEngine {
   private ctx: CanvasRenderingContext2D;
   private size = { width: 0, height: 0 };
@@ -7,10 +26,10 @@ export class VisualizationEngine {
     private canvas: HTMLCanvasElement,
     private container: HTMLDivElement
   ) {
-    const ctx = this.canvas.getContext('2d');
+    const ctx = this.canvas.getContext("2d");
 
     if (!ctx) {
-      throw new Error('Canvas context is not available');
+      throw new Error("Canvas context is not available");
     }
 
     this.ctx = ctx;
@@ -25,17 +44,24 @@ export class VisualizationEngine {
 
   private draw() {
     this.ctx.clearRect(0, 0, this.size.width, this.size.height);
-    this.ctx.fillStyle = '#fff';
+    this.ctx.fillStyle = "#242424";
     this.ctx.fillRect(0, 0, this.size.width, this.size.height);
+
+    // Draw the court lines
+    // Border
+    const lineWidth = this.feetToPixels(LINE_WIDTH);
+    this.ctx.lineWidth = lineWidth;
+    this.ctx.strokeStyle = "#ffffff";
+    this.ctx.strokeRect(
+      lineWidth / 2,
+      lineWidth / 2,
+      this.size.width - lineWidth * 2,
+      this.size.height - lineWidth * 2
+    );
   }
 
   private onResize() {
     const devicePixelRatio = window.devicePixelRatio || 1;
-
-    const COURT_LENGTH_FT = 94 / 2;
-    const COURT_WIDTH_FT = 50;
-
-    const COURT_ASPECT_RATIO = COURT_LENGTH_FT / COURT_WIDTH_FT;
 
     const width = this.container.clientWidth;
 
@@ -56,13 +82,16 @@ export class VisualizationEngine {
 
   private bindEvents() {
     window.addEventListener(
-      'resize',
+      "resize",
       () => {
-        console.log('resize');
         this.onResize();
         this.draw();
       },
       { signal: this.abortController.signal }
     );
+  }
+
+  private feetToPixels(feet: number) {
+    return feet * (this.size.width / COURT_LENGTH_FT);
   }
 }
