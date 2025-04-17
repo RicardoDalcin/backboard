0bfb3fe529fba44a52eb9540fce0b85fe5ff296d
Ricardo Dalcin <ricardo.dalcinh@gmail.com>
2025-04-16 23:51:02 -0300

    ✨ Draw regulation court markings

--- src/engine/Visualization.ts
+++ src/engine/Visualization.ts
@@ -6,16 +6,34 @@ const LINE_WIDTH = 0.16;
 // Distance between sideline and three-point line
 const THREE_POINT_LINE_DISTANCE = 3;
 
+// Real length is 14ft, but 14.15 aligns better
+const THREE_POINT_LINE_STRAIGHT_LENGTH = 14;
+
 // Radius of the three-point line from the basket
 const THREE_POINT_LINE_RADIUS = 23.75;
 
+const FREE_THROW_CIRCLE_RADIUS = 6;
+
 const BACKBOARD_DISTANCE_TO_BACKLINE = 4;
 const BASKET_DISTANCE_TO_BACKLINE = 5.25;
+const BASKET_RADIUS = 3 / 4;
 
-const COURT_LENGTH_FT = 94 / 2;
+const RESTRICTED_LINE_LENGTH = 15 / 12;
+const RESTRICTED_CIRCLE_RADIUS = 4;
+const RESTRICTED_AREA_WIDTH = 8;
+
+const BACKBOARD_WIDTH = 5;
+const BACKBOARD_DEPTH = 0.3;
+
+const PAINTED_AREA = {
+  width: 16,
+  length: 19,
+};
+
+const COURT_LENGTH_FT = 94 * 0.4;
 const COURT_WIDTH_FT = 50;
 
-const COURT_ASPECT_RATIO = COURT_LENGTH_FT / COURT_WIDTH_FT;
+const COURT_ASPECT_RATIO = COURT_WIDTH_FT / COURT_LENGTH_FT;
 
 export class VisualizationEngine {
   private ctx: CanvasRenderingContext2D;
@@ -58,6 +76,157 @@ export class VisualizationEngine {
       this.size.width - lineWidth * 2,
       this.size.height - lineWidth * 2
     );
+
+    // Three-point line (two straight lines and one arc)
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.feetToPixels(THREE_POINT_LINE_DISTANCE), 0);
+
+    this.ctx.lineTo(
+      this.feetToPixels(THREE_POINT_LINE_DISTANCE),
+      this.feetToPixels(THREE_POINT_LINE_STRAIGHT_LENGTH)
+    );
+
+    this.ctx.arc(
+      this.size.width / 2,
+      this.feetToPixels(BASKET_DISTANCE_TO_BACKLINE),
+      this.feetToPixels(THREE_POINT_LINE_RADIUS),
+      this.degToRad(180 - 22.5),
+      this.degToRad(360 + 22),
+      true
+    );
+
+    this.ctx.lineTo(
+      this.size.width - this.feetToPixels(THREE_POINT_LINE_DISTANCE),
+      0
+    );
+
+    this.ctx.stroke();
+
+    this.ctx.beginPath();
+    const backboardWidth = this.feetToPixels(BACKBOARD_WIDTH);
+    const backboardDistanceToBackline = this.feetToPixels(
+      BACKBOARD_DISTANCE_TO_BACKLINE
+    );
+
+    // Backboard
+    this.ctx.moveTo(
+      this.size.width / 2 - backboardWidth / 2,
+      backboardDistanceToBackline
+    );
+    this.ctx.lineTo(
+      this.size.width / 2 + backboardWidth / 2,
+      backboardDistanceToBackline
+    );
+    this.ctx.save();
+    this.ctx.lineWidth = this.feetToPixels(BACKBOARD_DEPTH);
+    this.ctx.stroke();
+    this.ctx.restore();
+
+    this.ctx.beginPath();
+    this.ctx.save();
+    this.ctx.globalAlpha = 0.15;
+    this.ctx.fillStyle = "#ffffff";
+    this.ctx.fillRect(
+      this.size.width / 2 - this.feetToPixels(PAINTED_AREA.width) / 2,
+      0,
+      this.feetToPixels(PAINTED_AREA.width),
+      this.feetToPixels(PAINTED_AREA.length)
+    );
+    this.ctx.restore();
+    this.ctx.strokeRect(
+      this.size.width / 2 - this.feetToPixels(PAINTED_AREA.width) / 2,
+      0,
+      this.feetToPixels(PAINTED_AREA.width),
+      this.feetToPixels(PAINTED_AREA.length)
+    );
+    this.ctx.stroke();
+
+    const basketRadius = this.feetToPixels(BASKET_RADIUS);
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(
+      this.size.width / 2,
+      this.feetToPixels(BACKBOARD_DISTANCE_TO_BACKLINE)
+    );
+    this.ctx.lineTo(
+      this.size.width / 2,
+      this.feetToPixels(BASKET_DISTANCE_TO_BACKLINE) - basketRadius
+    );
+
+    this.ctx.stroke();
+
+    this.ctx.beginPath();
+    this.ctx.arc(
+      this.size.width / 2,
+      this.feetToPixels(BASKET_DISTANCE_TO_BACKLINE),
+      basketRadius,
+      0,
+      2 * Math.PI
+    );
+    this.ctx.stroke();
+
+    this.ctx.beginPath();
+    this.ctx.arc(
+      this.size.width / 2,
+      this.feetToPixels(PAINTED_AREA.length),
+      this.feetToPixels(FREE_THROW_CIRCLE_RADIUS),
+      this.degToRad(0),
+      this.degToRad(180)
+    );
+    this.ctx.stroke();
+
+    this.ctx.beginPath();
+    this.ctx.arc(
+      this.size.width / 2,
+      this.feetToPixels(PAINTED_AREA.length),
+      this.feetToPixels(FREE_THROW_CIRCLE_RADIUS),
+      this.degToRad(180),
+      this.degToRad(360)
+    );
+    this.ctx.setLineDash([10, 10]);
+    this.ctx.stroke();
+    this.ctx.setLineDash([]);
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(
+      this.size.width / 2 - this.feetToPixels(RESTRICTED_AREA_WIDTH / 2),
+      this.feetToPixels(BACKBOARD_DISTANCE_TO_BACKLINE)
+    );
+
+    this.ctx.lineTo(
+      this.size.width / 2 - this.feetToPixels(RESTRICTED_AREA_WIDTH / 2),
+      this.feetToPixels(BACKBOARD_DISTANCE_TO_BACKLINE) +
+        this.feetToPixels(RESTRICTED_LINE_LENGTH)
+    );
+
+    this.ctx.arc(
+      this.size.width / 2,
+      this.feetToPixels(BASKET_DISTANCE_TO_BACKLINE),
+      this.feetToPixels(RESTRICTED_CIRCLE_RADIUS),
+      this.degToRad(180),
+      this.degToRad(360),
+      true
+    );
+
+    this.ctx.lineTo(
+      this.size.width / 2 + this.feetToPixels(RESTRICTED_AREA_WIDTH / 2),
+      this.feetToPixels(BACKBOARD_DISTANCE_TO_BACKLINE)
+    );
+
+    this.ctx.stroke();
+
+    this.ctx.beginPath();
+    this.ctx.strokeRect(
+      this.size.width / 2 - this.feetToPixels(FREE_THROW_CIRCLE_RADIUS),
+      0,
+      this.feetToPixels(2 * FREE_THROW_CIRCLE_RADIUS),
+      this.feetToPixels(PAINTED_AREA.length)
+    );
+    this.ctx.stroke();
+  }
+
+  private degToRad(deg: number) {
+    return deg * (Math.PI / 180);
   }
 
   private onResize() {
@@ -75,8 +244,6 @@ export class VisualizationEngine {
       height: width / COURT_ASPECT_RATIO,
     };
 
-    console.log(this.size);
-
     this.ctx.scale(devicePixelRatio, devicePixelRatio);
   }
 
@@ -92,6 +259,6 @@ export class VisualizationEngine {
   }
 
   private feetToPixels(feet: number) {
-    return feet * (this.size.width / COURT_LENGTH_FT);
+    return feet * (this.size.width / COURT_WIDTH_FT);
   }
 }
