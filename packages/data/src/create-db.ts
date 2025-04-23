50c2801dd326c1cd0e41657be84641e2d0284293
Ricardo Dalcin <ricardo.dalcinh@gmail.com>
2025-04-23 08:51:33 -0300

    ✨ Improve DB creation script

--- packages/data/src/create-db.ts
+++ packages/data/src/create-db.ts
@@ -1,10 +1,16 @@
 import sqlite3 from 'sqlite3';
-import { rm } from 'fs/promises';
+import { existsSync } from 'fs';
+import { rm, mkdir } from 'fs/promises';
 
-const DB_PATH = './output/test.sqlite';
+const FOLDER_PATH = './output';
+const DB_PATH = `${FOLDER_PATH}/nba.sqlite3`;
 
 async function createDb() {
-  await rm(DB_PATH, { force: true });
+  if (!existsSync(FOLDER_PATH)) {
+    await mkdir(FOLDER_PATH);
+  } else {
+    await rm(DB_PATH, { force: true });
+  }
 
   const db = new sqlite3.Database(DB_PATH);
 
@@ -16,3 +22,5 @@ async function createDb() {
 
   db.close();
 }
+
+createDb();
