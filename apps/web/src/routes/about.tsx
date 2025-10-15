63fc4deaf6185e142e3b3b38f32f5988852286df
Ricardo Dalcin <ricardo.dalcinh@gmail.com>
2025-10-15 07:52:12 -0300

    ✨ About page

--- apps/web/src/routes/about.tsx
+++ apps/web/src/routes/about.tsx
@@ -1,4 +1,3 @@
-import { useWorker } from '@/server/db/test';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/about')({
@@ -6,13 +5,31 @@ export const Route = createFileRoute('/about')({
 });
 
 function About() {
-  const { loadData, interrupt } = useWorker();
-
   return (
-    <div>
-      Hello from About!
-      <button onClick={loadData}>Load Data</button>
-      <button onClick={interrupt}>Interrupt</button>
+    <div className="w-full h-full flex items-center justify-center flex-col gap-12 pb-[20vh]">
+      <h1 className="text-4xl font-semibold">About Backboard</h1>
+
+      <div className="max-w-xl text-justify flex items-center justify-center flex-col gap-4 text-muted-foreground">
+        <p className="text-lg">
+          Backboard in an offline-first data visualization dashboard for
+          exploring, analyzing and comparing NBA shot data.
+        </p>
+
+        <p className="text-lg">
+          It is entirely open-source, so feel free to check out the{' '}
+          <a
+            href="https://github.com/RicardoDalcin/backboard"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-sky-600 hover:text-sky-500 transition-colors"
+          >
+            source code
+          </a>{' '}
+          and contribute to the project. The stack used is React, Tailwind and
+          to make offline fast queries possible the WASM port of SQLite was used
+          with the Origin-Private File System API.
+        </p>
+      </div>
     </div>
   );
 }
